40362c72c3151d2ef2e7eb3d15a2a68397cc7fe4
npati05 <niveditapatikar2021@gmail.com>
2024-05-26 21:36:55 +0530

    Modified by AK develop bracnch

--- List.py
+++ List.py
@@ -7,3 +7,5 @@ new_dict = {i:college[i] for i in range(len(college))}
 print(new_dict)
 
 UNIVERSITY = ["BURDWAN", "KOLKATA", "WBUT"]
+new_dict = {i:college[i] for i in range(len(college))} ## new added
+print(new_dict)
